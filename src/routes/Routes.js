dfe41007729c4746b4ee2e7981d8c70b5fb9dace
Avijit Karmaker <ajkacca@gmail.com>
2022-11-06 00:00:08 +0200

    update routes and pages

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import About from '../pages/About';
+import Products from '../pages/Products';
+import SingleProduct from '../pages/SingleProduct';
+import Error from '../pages/Error';
 
 const AppRoutes = () => {
   return (
@@ -9,6 +12,10 @@ const AppRoutes = () => {
         <Routes>
             <Route path='/' element={<Home/>} />
             <Route path='about' element={<About/>} />
+            <Route path='products' element={<Products/>} />
+            <Route path='products/:id' element={<SingleProduct/>} />
+            <Route path='*' element={<Error/>} />
+
         </Routes>
     </>
   )
