0a9531567e2dde34767b57bca58701229361b870
Avijit Karmaker <ajkacca@gmail.com>
2022-11-05 23:25:48 +0200

    add tailwind config

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# Techcommerce_Front
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className='w-full bg-indigo-300'>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
