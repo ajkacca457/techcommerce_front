0a976ccfe7439ffb5c11645777c6d1e5bfeea7df
Avijit Karmaker <ajkacca@gmail.com>
2022-11-09 19:24:08 +0200

    update reducer,state

--- src/context/ProductContext.js
+++ src/context/ProductContext.js
@@ -1,14 +1,41 @@
-import { useContext } from "react";
+import axios from "axios";
+import { useContext, useEffect, useReducer } from "react";
 import {createContext} from "react";
+import { GET_PRODUCTS_BEGINS,GET_PRODUCTS_SUCCESS,GET_PRODUCTS_ERROR } from "./actions";
+import ProductReducer from "./reducers/ProductReducer";
+
 
 const ProductContext= createContext();
 
+const roolUrl="https://techcommerce-api.netlify.app/api/products";
+
 const initialState= {
-    name:"hello",
-    addres:"121 downing street"
+    products: [],
+    featured:[],
+    loading: false,
+    error: false
 }
 
 const ProductProvider=({children})=>{
+    const [state,dispatch]= useReducer(ProductReducer,initialState);
+    console.log(state);
+
+const fetchProducts= async (url)=>{
+    dispatch({type:GET_PRODUCTS_BEGINS});
+    try {
+        const response= await axios.get(url);
+        const results= response.data;
+        dispatch({type:GET_PRODUCTS_SUCCESS,payload:results})
+    } catch (error) {
+        dispatch({type:GET_PRODUCTS_ERROR})
+    }
+}    
+
+useEffect(()=>{
+    fetchProducts(roolUrl);
+},[])
+
+
 return(
 <ProductContext.Provider value={initialState}>
     {children}
--- src/context/reducers/ProductReducer.js
+++ src/context/reducers/ProductReducer.js
@@ -3,6 +3,14 @@ import { GET_PRODUCTS_BEGINS,GET_PRODUCTS_SUCCESS, GET_PRODUCTS_ERROR } from "..
 
 const ProductReducer=(state,action)=> {
     switch (action.type) {
+        case GET_PRODUCTS_BEGINS:
+            return {...state,loading:true};
+        case GET_PRODUCTS_SUCCESS:
+            return {
+                ...state,
+                loading:false,
+                products:action.payload
+            };
     }
 
 }
