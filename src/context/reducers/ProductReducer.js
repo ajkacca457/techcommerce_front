a4c24476793dd33d2d2d40b430eb5dff9d52fc49
Avijit Karmaker <ajkacca@gmail.com>
2022-11-09 19:29:27 +0200

    update reducer

--- src/context/reducers/ProductReducer.js
+++ src/context/reducers/ProductReducer.js
@@ -6,11 +6,20 @@ const ProductReducer=(state,action)=> {
         case GET_PRODUCTS_BEGINS:
             return {...state,loading:true};
         case GET_PRODUCTS_SUCCESS:
+            const featureProducts= action.payload.filter(item=> item.Featured===true);
             return {
                 ...state,
                 loading:false,
-                products:action.payload
+                products:action.payload,
+                featured:featureProducts
             };
+        case GET_PRODUCTS_ERROR:
+            return {
+                ...state,
+                error:true
+            }
+        default:
+            return {...state}
     }
 
 }
