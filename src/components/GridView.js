311f4ec340293c1ce3a412768315585977b0ff23
Avijit karmaker <ajkacca@gmail.com>
2022-11-10 17:27:32 +0200

    update grid view

--- src/components/GridView.js
+++ src/components/GridView.js
@@ -1,8 +1,20 @@
 import React from 'react'
 
-const GridView = () => {
+const GridView = ({products}) => {
   return (
-    <div>GridView</div>
+    <div className='grid grid-cols-6 gap-x-[10px]'>
+       {products && products.map((item)=>{
+        return(
+            <div className='col-span-2 my-4'>
+            <img src={item.Image[0].url} className="w-[150px] h-[100px]"/>
+            <div>
+                <p>{item.Name}</p>
+                <p>{item.Company}</p>    
+            </div>
+            </div>
+        )
+       })}     
+    </div>
   )
 }
 
