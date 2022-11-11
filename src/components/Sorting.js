5ef34a339eddade3d48a98089f626aac46109317
Avijit Karmaker <ajkacca@gmail.com>
2022-11-11 15:18:45 +0200

    update sorting

--- src/components/Sorting.js
+++ src/components/Sorting.js
@@ -3,7 +3,7 @@ import {BsFillGrid1X2Fill, BsListUl} from 'react-icons/bs';
 import { useFilterContext } from '../context/FilterContext';
 
 const Sorting = () => {
-    const {changeDisplayList,changeDisplayGrid}=useFilterContext();
+    const {filtered_products,changeDisplayList,changeDisplayGrid}=useFilterContext();
 
   return (
     <div className='bg-orange-300 grid grid-cols-6'>
@@ -11,6 +11,18 @@ const Sorting = () => {
             <BsFillGrid1X2Fill size={25} onClick={changeDisplayGrid}/>
             <BsListUl size={25} onClick={changeDisplayList}/> 
         </div>
+        <div className='col-span-2'>
+          {filtered_products.length} products found
+        </div>
+        <form className='col-span-2'>
+          <select name="sort" id="sort">
+            <option value="price-lowest">price-lowest</option>
+            <option value="price-highest">price-highest</option>
+            <option value="name-a">name(a-z)</option>
+            <option value="name-z">name(z-a)</option>
+          </select>
+        </form>
+
     </div>
   )
 }
