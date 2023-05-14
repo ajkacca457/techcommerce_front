cae4d4572eaf1e77b65fe03c1c4349cade24b1bd
Avijit Karmaker <ajkacca@gmail.com>
2023-05-14 18:58:25 +0300

    add new font

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App flex flex-col justify-between">
+    <div className="App flex flex-col justify-between font-work">
         <Container/>
         <Footer/>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        work: ['Work Sans', "sans-serif"],
+       },
+    },
   },
   plugins: [],
 }
