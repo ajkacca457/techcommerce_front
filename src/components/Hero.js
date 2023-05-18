7aa370a7203497886e0f203fae059ec45eb44884
Avijit Karmaker <ajkacca@gmail.com>
2023-05-18 23:32:49 +0300

    remove hero lorem ipsum

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,8 +6,8 @@ const Hero = ({align}) => {
     <div className='grid grid-cols-2 place-items-center p-8 gap-x-6'>
         <div className='col-span-1 text-left'>
             <h3> Here will be subheading</h3>
-            <h1>Here will the heading for Title</h1>
-            <p>Lorem Ipsum Dolor Sit Amet, Consectetur Adipisicing Elit, Sed Do Eiuiana Smod Tempor Ut Labore Et Dolore Magna Aliqua. Ut Enim Ad Minim Veniam, Quis Nostrud Exercitation Ullamco Laboris Nisi Ut Aliquip.</p>
+            <h1>Heading</h1>
+            <p>All description</p>
             <h3>Price in $</h3>
             <button className='bg-orange-400 px-3 py-1 text-white'>Add to cart</button>
         </div> 
