9b1eeec611068e91fa22ecfc764e141d6dd2fd31
Avijit Karmaker <ajkacca@gmail.com>
2023-06-19 11:19:29 +0300

    update hero styling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Hero = ({image}) => {
   return (
-    <div className='flex flex-col justify-center bg-center bg-cover min-h-[70vh] my-12 relative' style={{backgroundImage:`url(${image})`}}>
-      <div className='w-full h-full bg-black absolute top-0 left-0 opacity-50 z-0'></div>
+    <div className='flex flex-col justify-center bg-center bg-cover min-h-[70vh] my-12 relative group' style={{backgroundImage:`url(${image})`}}>
+      <div className='w-full h-full bg-black absolute top-0 left-0 opacity-40 z-0 transition ease-in duration-200 group-hover:opacity-60'></div>
       <div className='text-white w-1/2 mx-auto z-10'>
         <h3 className='text-3xl uppercase'> Here will be subheading</h3>
         <p>All description</p>
