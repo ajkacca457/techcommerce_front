bfc95bf44712065005e639fa7993d6e6fc1d1ddc
Avijit Karmaker <ajkacca@gmail.com>
2023-06-19 11:45:36 +0300

    update hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const Hero = ({image, details:{heading="Demo Text",description="Demo Text"}}) => {
   return (
-    <div className='flex flex-col justify-center bg-center bg-cover min-h-[70vh] my-12 relative group' style={{backgroundImage:`url(${image})`}}>
+    <div className='flex flex-col justify-center bg-center bg-cover min-h-[70vh] mt-6 mb-12 relative group' style={{backgroundImage:`url(${image})`}}>
       <div className='w-full h-full bg-black absolute top-0 left-0 opacity-40 z-0 transition ease-in duration-200 group-hover:opacity-60'></div>
       <div className='text-white w-1/2 mx-auto z-10'>
         <h3 className='text-4xl uppercase'> {heading}</h3>
