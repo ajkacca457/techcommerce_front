fbcf9d6fc401c482342128f8f3b13375de8c99b5
Avijit Karmaker <ajkacca@gmail.com>
2023-06-19 11:44:01 +0300

    update about hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
 
-const Hero = ({image, details:{heading,description}}) => {
+const Hero = ({image, details:{heading="Demo Text",description="Demo Text"}}) => {
   return (
     <div className='flex flex-col justify-center bg-center bg-cover min-h-[70vh] my-12 relative group' style={{backgroundImage:`url(${image})`}}>
       <div className='w-full h-full bg-black absolute top-0 left-0 opacity-40 z-0 transition ease-in duration-200 group-hover:opacity-60'></div>
       <div className='text-white w-1/2 mx-auto z-10'>
-        <h3 className='text-3xl uppercase'> {heading?heading:"Demo Text"}</h3>
-        <p>{description?description:"Demo text"}</p>
+        <h3 className='text-4xl uppercase'> {heading}</h3>
+        <p className='my-6'>{description}</p>
         <NavLink to="/products" className="bg-orange-400 uppercase px-3 py-2 block w-fit mx-auto rounded transition duration-300 hover:bg-orange-600">See products</NavLink>
       </div> 
     </div> 
--- src/pages/About.js
+++ src/pages/About.js
@@ -4,9 +4,13 @@ import HeroImg2 from "../images/hero2.jpg"
 
 
 const About = () => {
+  const details= {
+    heading:"A shop for your Tech",
+    description:"Tech-commerce promises best deals, best prices and best service" 
+  };
   return (
     <div className='max-w-content mx-auto'>
-      <Hero image={HeroImg2}/>
+      <Hero image={HeroImg2} details={details}/>
     </div>
   )
 }
