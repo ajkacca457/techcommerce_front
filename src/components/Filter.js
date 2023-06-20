0bff1c158ca546d61d6d773dd1e787dd141ebeb5
Avijit Karmaker <ajkacca@gmail.com>
2023-06-20 23:27:33 +0300

    update filter inputs

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -11,13 +11,13 @@ const Filter = () => {
     <>
       <form onSubmit={(e)=> e.preventDefault()} className='mt-8'>
         <div className="form-group mb-4">
-          <label htmlFor="searchtext">Search Products</label>
-          <input type="text" name="searchtext" className='border-2 border-slate-300' value={searchtext} onChange={updateFilters}/>
+          <label htmlFor="searchtext" className='text-sm'>Search Products</label>
+          <input type="text" name="searchtext" className='border-[1px] rounded border-slate-300 w-full px-2' value={searchtext} onChange={updateFilters}/>
         </div>
 
         <div className="form-group my-4">
-          <label htmlFor="company">Select Company</label>
-          <select name="company" id="company" className='w-full border-2 border-slate-300' value={company} onChange={updateFilters}>
+          <label htmlFor="company" className='text-sm'>Select Company</label>
+          <select name="company" id="company" className='w-full border-[1px] rounded border-slate-300 px-2' value={company} onChange={updateFilters}>
             {companies && companies.map((item,index)=>{
               return(<option value={item} key={index}>{item}</option>)
             })}
@@ -25,8 +25,8 @@ const Filter = () => {
         </div>
 
         <div className="form-group my-4">
-          <label htmlFor="category">Select Category</label>
-          <select name="category" id="category" className='w-full border-2 border-slate-300' value={category} onChange={updateFilters}>
+          <label htmlFor="category" className='text-sm'>Select Category</label>
+          <select name="category" id="category" className='w-full border-[1px] rounded border-slate-300 px-2' value={category} onChange={updateFilters}>
             {categories && categories.map((item,index)=>{
               return(<option value={item} key={index}>{item}</option>)
             })}
@@ -34,16 +34,16 @@ const Filter = () => {
         </div>
 
         <div className="form-group my-4">
-          <label htmlFor="price">Current price ${price}</label>
+          <label htmlFor="price" className='text-sm'>Current price ${price}</label>
           <input type="range" name="price" value={price} min={minPrice} max={maxPrice} onChange={updateFilters} />
         </div>
 
-        <div className="form-group my-4">
-          <label htmlFor="freeshipping">Shipping</label>
+        <div className="form-group my-4 flex justify-center items-center gap-x-4">
+          <label htmlFor="freeshipping" className='text-sm'>Shipping</label>
           <input type="checkbox" name="freeshipping" checked={freeshipping} onChange={updateFilters} />
         </div>
 
-        <button className='bg-red-600 px-2 py-1 text-white hover:bg-red-400' onClick={clearFilter}>Clear Filters</button>
+        <button className='bg-red-500 px-2 py-1 text-white transition duration-300 hover:bg-red-600 rounded' onClick={clearFilter}>Clear Filters</button>
 
 
       </form>
