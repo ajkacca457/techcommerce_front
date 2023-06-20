4a1a30923b0ef2b7a887ba5fe1cb7f8d46e0f069
Avijit Karmaker <ajkacca@gmail.com>
2023-06-20 23:18:05 +0300

    update grid view for sorting

--- src/components/Sorting.js
+++ src/components/Sorting.js
@@ -5,18 +5,17 @@ import { useFilterContext } from '../context/FilterContext';
 
 const Sorting = () => {
     const {filtered_products,changeDisplayList,changeDisplayGrid,sort,sortUpdate, list_view}=useFilterContext();
-
   return (
-    <div className='grid grid-cols-6 mt-8 place-items-center bg-slate-400  py-2 rounded'>
+    <div className='grid grid-cols-6 mt-8 bg-slate-400  py-2 px-4 rounded'>
         <div className='col-span-2 flex gap-x-[10px] items-center'>
             <BsFillGrid1X2Fill size={20} onClick={changeDisplayGrid} className={!list_view?`text-white border-[1px]`: `text-black border-[1px] border-black`} />
             <GoListUnordered size={20} onClick={changeDisplayList} className={list_view?`text-white border-[1px]`: `text-black border-[1px] border-black`}/> 
         </div>
-        <div className='col-span-2'>
+        <div className='col-span-2 uppercase text-sm'>
           {filtered_products.length} products found
         </div>
         <form className='col-span-2'>
-          <select name="sort" id="sort" value={sort} onChange={sortUpdate}>
+          <select name="sort" id="sort" value={sort} onChange={sortUpdate} className='w-full'>
             <option value="price-lowest">price-lowest</option>
             <option value="price-highest">price-highest</option>
             <option value="name-a">name(a-z)</option>
