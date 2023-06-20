1609777dde6123eda419e0faad5c37d7c4f97b58
Avijit Karmaker <ajkacca@gmail.com>
2023-06-20 13:54:27 +0300

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import {FiSmartphone} from "react-icons/fi"
 
 const Footer = () => {
   return (
-    <div className='w-full bg-indigo-300 py-8'>This is the footer</div>
+    <div className='w-full bg-slate-700 text-white py-4'>
+      <p>©Tech Commerce group.</p>
+      <div className='flex items-center justify-center gap-x-1'><FiSmartphone className='text-white'/> +3580465200200</div>
+    </div>
   )
 }
 
