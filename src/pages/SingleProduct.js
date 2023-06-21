5da7df58e706c5f5729953fd1e91cc65acf7f6af
Avijit Karmaker <ajkacca@gmail.com>
2023-06-21 12:16:04 +0300

    add responsivness for list view

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -26,14 +26,14 @@ const SingleProduct = () => {
     if(fields) {
       const {Category,Company,Description,Featured,FreeShipping,Image,Name,Price,Rating,Stock}=fields;
     return (
-    <div className='min-h-[80vh] flex flex-col justify-center'>
+    <div className='min-h-[80vh] flex flex-col justify-center px-8 my-10'>
     <div className='grid grid-cols-2 gap-x-4 max-w-content mx-auto place-items-center'>
-      <div className='col-span-1'>
+      <div className='col-span-full md:col-span-1'>
         {Image.map((item,index)=> {
-          return(<img src={item.url} key={index} className='w-1/2 mx-auto' alt={Name}/>)
+          return(<img src={item.url} key={index} className='w-1/2 mx-auto mb-6' alt={Name}/>)
         })}
       </div>
-      <div>
+      <div className='col-span-full md:col-span-1'>
         <h3 className='text-xl font-semibold border-b-[1px] pb-2'>{Name}</h3>
         <p className='text-sm my-6 border-b-[1px] pb-6'>{Description}</p>
         <p className='text-sm my-2'>Gadget type: <span className='bg-slate-500 px-2 py-1 text-white rounded'>{Category}</span></p>
